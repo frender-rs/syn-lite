b162da3836841db88ff79a1ccbf6cf7d1d54b578
Equal Ma <equalma@outlook.com>
2023-03-18 20:52:55 +0800

    feat: expand_or and expand_if_else

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 #![no_std]
 
 #[macro_export]
+macro_rules! expand_or {
+    ([] $($or:tt)*  ) => { $($or)* };
+    ([$($expand:tt)+] $($or:tt)* ) => { $($expand)+ };
+}
+
+#[macro_export]
+macro_rules! expand_if_else {
+    ([] $then:tt {$($else:tt)*}  ) => { $($else)* };
+    ([$($if:tt)+] {$($then:tt)*} $else:tt ) => { $($then)* };
+}
+
+#[macro_export]
 macro_rules! parse_inner_attrs {
     (
         $([ $($before:tt)* ])?
