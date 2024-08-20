b3e4180a8bc94bb0a1e7da1e580f82def66cc707
Equal Ma <equalma@outlook.com>
2024-08-20 23:59:59 +0800

    feat!: remove parse_where_clause!

--- tests/where_clause.rs
+++ tests/where_clause.rs
@@ -1,25 +1,26 @@
-use syn_lite::parse_where_clause;
+use syn_lite::consume_optional_where_clause;
 
 macro_rules! assert_no_where {
     (
-        where_clause! {}
-        rest! {}
+        rest {}
     ) => {};
 }
 
-parse_where_clause! {
-    {} => assert_no_where!
+consume_optional_where_clause! {
+    on_finish { assert_no_where! }
+    input {}
 }
 
 macro_rules! assert_only_where {
     (
-        where_clause! { where }
-        rest! {}
+        where_clause { where }
+        rest {}
     ) => {};
 }
 
-parse_where_clause! {
-    {where} => assert_only_where!
+consume_optional_where_clause! {
+    on_finish {assert_only_where!}
+    input {where}
 }
 
 #[test]
@@ -31,14 +32,14 @@ fn many() {
     let t_path;
     macro_rules! assert_many {
         (
-            where_clause! {
+            where_clause {
                 where
                     'a: 'b + 'c,
                     'b: 'c,
                     $r_ty:ty : ? $sized:tt + $r_path:path,
                     for<'a> $t_ty:ty : $t_path:path
             }
-            rest! { ; }
+            rest { ; }
         ) => {
             r_ty = stringify!($r_ty);
             sized = stringify!($sized);
@@ -48,15 +49,16 @@ fn many() {
         };
     }
 
-    parse_where_clause! {
-        {
+    consume_optional_where_clause! {
+        on_finish {assert_many!}
+        input {
             where
                 'a: 'b + 'c,
                 'b: 'c,
                 R: ?Sized + AsRef<str>,
                 for<'a> T: FnOnce(&'a str)
             ;
-        } => assert_many!
+        }
     }
 
     assert_eq!(r_ty, "R");
@@ -75,14 +77,14 @@ fn many_trailing() {
     let t_path;
     macro_rules! assert_many_trailing {
         (
-            where_clause! {
+            where_clause {
                 where
                     'a: 'b + 'c,
                     'b: 'c,
                     $r_ty:ty : ? $sized:tt + $r_path:path,
                     for<'a> $t_ty:ty : $t_path:path,
             }
-            rest! { {} }
+            rest { {} }
         ) => {
             r_ty = stringify!($r_ty);
             sized = stringify!($sized);
@@ -92,15 +94,16 @@ fn many_trailing() {
         };
     }
 
-    parse_where_clause! {
-        {
+    consume_optional_where_clause! {
+        on_finish {assert_many_trailing!}
+        input {
             where
                 'a: 'b + 'c,
                 'b: 'c,
                 R: ?Sized + AsRef<str>,
                 for<'a> T: FnOnce(&'a str),
             {}
-        } => assert_many_trailing!
+        }
     }
 
     assert_eq!(r_ty, "R");
