9b2c272671041e708371176003c7930a09d3ed8b
Equal Ma <equalma@outlook.com>
2024-08-21 01:45:04 +0800

    feat!: remove parse_inner_attrs!

--- src/lib.rs
+++ src/lib.rs
@@ -12,39 +12,6 @@ macro_rules! expand_if_else {
     ([$($if:tt)+] {$($then:tt)*} $else:tt ) => { $($then)* };
 }
 
-#[macro_export]
-macro_rules! parse_inner_attrs {
-    (
-        $([ $($before:tt)* ])?
-        {
-            $(#!$inner_attr:tt)+
-            $($rest:tt)*
-        }
-        $([ $($after:tt)* ])?
-        => $($out_macro_and_bang:tt)+
-    ) => {
-        $($out_macro_and_bang)+ {
-            $($($before)*)?
-            inner_attrs! { $(#!$inner_attr)+ }
-            rest! { $($rest)* }
-            $($($after)*)?
-        }
-    };
-    (
-        $([ $($before:tt)* ])?
-        { $($rest:tt)* }
-        $([ $($after:tt)* ])?
-        => $($out_macro_and_bang:tt)+
-    ) => {
-        $($out_macro_and_bang)+ {
-            $($($before)*)?
-            inner_attrs! {}
-            rest! { $($rest)* }
-            $($($after)*)?
-        }
-    };
-}
-
 // region: common utils
 
 #[doc(hidden)]
--- tests/inner_attrs.rs
+++ tests/inner_attrs.rs
@@ -1,16 +1,40 @@
+mod asserts {
+    macro_rules! one_token {
+        ($t:tt) => {
+            1
+        };
+    }
+    macro_rules! token_count {
+        ($($t:tt)*) => {
+
+            0 $(+ one_token! {$t})*
+        };
+    }
+
+    const _: () = {
+        assert!(token_count! {#![cfg(..)]} == 3);
+        assert!(
+            token_count! {
+                //! hello
+            } == 3
+        );
+    };
+}
+
 macro_rules! assert_no_inner_attrs {
     (
-        inner_attrs! {}
-        rest! {}
+        inner_attrs {}
+        rest {}
     ) => {};
 }
 
-syn_lite::parse_inner_attrs! {
-    {} => assert_no_inner_attrs!
+syn_lite::consume_inner_attrs! {
+    on_finish {assert_no_inner_attrs!}
+    input {}
 }
 
 #[test]
-fn full() {
+fn one() {
     let parsed;
     let rest;
     let before;
@@ -18,10 +42,10 @@ fn full() {
 
     macro_rules! assert_no_inner_attrs {
         (
-            before! { $($before:tt)* }
-            inner_attrs! {#![inner = $inner:literal]}
-            rest! { $($rest:tt)* }
-            after! { $($after:tt)* }
+            before { $($before:tt)* }
+            inner_attrs {#![inner = $inner:literal]}
+            rest { $($rest:tt)* }
+            after { $($after:tt)* }
         ) => {
             $($before)*
             parsed = $inner;
@@ -30,14 +54,55 @@ fn full() {
         };
     }
 
-    syn_lite::parse_inner_attrs! {
-        [before! { before = true; }]
-        {
+    syn_lite::consume_inner_attrs! {
+        on_finish {assert_no_inner_attrs!}
+        prepend { before { before = true; } }
+        input {
             #![inner = true]
             rest = true;
         }
-        [after! { after = true; }]
-        => assert_no_inner_attrs!
+        append { after { after = true; } }
+    }
+
+    assert!(parsed);
+    assert!(rest);
+    assert!(before);
+    assert!(after);
+}
+
+#[test]
+fn two() {
+    let parsed;
+    let rest;
+    let before;
+    let after;
+
+    macro_rules! assert_no_inner_attrs {
+        (
+            before { $($before:tt)* }
+            inner_attrs {
+                #![inner = $inner:literal]
+                #![cfg(..)]
+            }
+            rest { $($rest:tt)* }
+            after { $($after:tt)* }
+        ) => {
+            $($before)*
+            parsed = $inner;
+            $($rest)*
+            $($after)*
+        };
+    }
+
+    syn_lite::consume_inner_attrs! {
+        on_finish {assert_no_inner_attrs!}
+        prepend { before { before = true; } }
+        input {
+            #![inner = true]
+            #![cfg(..)]
+            rest = true;
+        }
+        append { after { after = true; } }
     }
 
     assert!(parsed);
